f37b6edac2aa2394daa3d1f5b110a7bbba845273
Batsy <anggiaputri4@gmail.com>
2020-07-16 22:43:04 +0700

    edit on another laptop

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-// error_reporting(0);
-if(isset($_POST['submit'])){
+error_reporting(0);
+if($_POST['submit']){
 	session_start();
 	if(md5($_POST['pin']) == $_SESSION['image_random_value']){
 		echo '<p>Kode Benar</p>';
@@ -13,7 +13,7 @@ if(isset($_POST['submit'])){
 <form name="form1" method="post" action="">
 <img src="captcha.php">
 <div>
-Masukkan Kode Security diatas : <br><input type="text" name="pin">
+Masukkan Kode Security diatas anda : <br><input type="text" name="pin">
 <input type="submit" name="submit" value="Submit">
 </div>
 </form>
